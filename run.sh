4cfe11979ae32234aeab6f4ae8227c4b74bf904a
0htjit.eth <95016877+0htjit@users.noreply.github.com>
2023-11-15 18:25:10 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-    response=$(curl -k -f https://www.pilotdog.tech/api/get_eligible_proof?address=0xfAF286d6BAB6B84ADa3162439734f419018BC77A 2>&1)
+    response=$(curl -k -f https://www.pilotdog.tech/api/get_eligible_proof?address=0x5A6888e5A95D579Cf85614c124d21bCbD5418A0C 2>&1)
     if [[ $response == *"502 Bad Gateway"* ]]; then
         echo "Received Bad Gateway error. Retrying in 60 seconds..."
         sleep 60
